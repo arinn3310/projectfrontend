7a8cb40bf696dddc3f8e17410b44006d81b23b98
arinn3310 <arinupadhyay10@gmail.com>
2024-09-24 10:32:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { RouterProvider } from 'react-router-dom'
+import Router from "./router/router"
+import { Provider } from "react-redux"
+import store from "./redux/store"
+
+function App() {
+
+  return (
+    <Provider store={store}>
+    <div className="w-screen h-screen m-0 p-0 flex justify-center items-center">
+      <RouterProvider router={Router} />
+    </div>
+    </Provider>
+  )
+}
+
+export default App
